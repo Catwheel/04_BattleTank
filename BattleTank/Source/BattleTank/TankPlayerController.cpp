3afc15ae0d1f1e1185498e908ca1764f074bd5cc
Catwheel <sbtanker@gmail.com>
2020-03-29 01:08:03 -0400

    add a logging system to validate whether or not the player controller has a controlled tank

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,6 +2,21 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin play"));
+
+	ATank* tempTank = GetControlledTank();
+	if (tempTank != nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController controlling %s"), *(tempTank->GetName()));
+	}
+	else
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController NOT controlling a tank!"));
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
