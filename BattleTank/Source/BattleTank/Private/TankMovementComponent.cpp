3fb8a51e1b9c4d4513a184fb4450418fb8961c86
Catwheel <sbtanker@gmail.com>
2020-05-27 20:05:21 -0400

    Updating project settings with some copyright info and doing some coding tweaks, including setting the right visibility to for functions, putting categories in quotes for safety reason, and un-wiring the manuel input controls

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -14,7 +14,6 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
-	// TODO prevent double speed due to dual control use
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
@@ -22,9 +21,6 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
-
-	UE_LOG(LogTemp, Warning, TEXT("%s is turning"), *GetOwner()->GetName());
-	// TODO prevent double speed due to dual control use
 }
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
