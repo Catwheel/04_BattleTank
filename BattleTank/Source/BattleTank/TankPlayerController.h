cb1e0b3e36781edb334dbdc5e4d17596e9199a0a
Catwheel <sbtanker@gmail.com>
2020-07-07 20:54:30 -0400

    Hooked up the tank aiming component refernce system using a blue print event. Also changed the color of the locked state to green

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -7,9 +7,10 @@
 #include "TankPlayerController.generated.h"
 
 class ATank;
+class UTankAimingComponent;
 
 /**
- * 
+ * Resonsible for helping the player tank aim
  */
 UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
@@ -19,6 +20,9 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 protected:
 	UFUNCTION(BlueprintCallable, Category = "Setup")
 	ATank* GetControlledTank() const;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Setup")
+	void FoundAimingComponent(UTankAimingComponent* AimCompRed);
 private:
 	virtual void BeginPlay() override;
 	virtual void Tick( float DeltaTime) override;
