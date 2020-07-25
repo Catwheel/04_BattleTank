40448cdf675a10c105b451ee6b38ab81453418a6
Catwheel <sbtanker@gmail.com>
2020-07-24 20:48:23 -0400

    Added some sideways friction. May want to tune it further but it works and you no longer slide all over the place

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,6 +2,20 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = TankRoot->GetMass() * CorrectionAcceleration / 3;
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
 void UTankTrack::SetThrottle(float throttle)
 {
 	// TODO clamp actual throttle value so player can't over-drive
